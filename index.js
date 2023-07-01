61c63445f37601292c8235119cadeefeae56d029
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-01 23:21:34 +0000

    updated the codes and simple routes implemented

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [{
+        id: 0,
+        name: 'Kofi',
+    },
+    {
+        id: 1,
+        name: 'kwame',
+    },
+    {
+        id: 2,
+        name: 'Kwesi',
+    }
+]
+
+
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
+    const items = req.url.split('/');
+    if (items[1] === 'friends') {
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
         res.statusCode = 200;
         res.setHeader('COntent-Type', 'application/json');
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Sir Isaac Newton',
-        }));
-    } else if (req.url === '/messages') {
+        if (items.length == 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
