269cba6847bd80c69c1a86f6bbe97b6d8ce7353f
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-01 22:41:31 +0000

    initial commit

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
+    if (req.url === '/friends') {
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        // });
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'Sir Isaac Newton',
+        }));
+    } else if (req.url === '/messages') {
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<li>Hello Kojo!</li>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
+    server.listen(PORT, () => {
+        console.log(`Listening to port ${PORT}...`);
     });
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'Sir Isaac Newton',
-    }));
-});
-
-server.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}...`);
 });
