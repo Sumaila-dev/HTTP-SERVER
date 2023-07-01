68d5ab310007d3bdf5ca80545d1120e33c500081
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-01 22:54:56 +0000

    updated the server

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer();
+
+server.on('request', (req, res) => {
     if (req.url === '/friends') {
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
+        res.statusCode = 200;
+        res.setHeader('COntent-Type', 'application/json');
         res.end(JSON.stringify({
             id: 1,
             name: 'Sir Isaac Newton',
         }));
     } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
         res.write('<li>Hello Kojo!</li>');
@@ -19,7 +24,8 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         res.end();
     }
-    server.listen(PORT, () => {
-        console.log(`Listening to port ${PORT}...`);
-    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}...`);
 });
