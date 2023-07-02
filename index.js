24adb0791f47a916b3a0e61d776c9854d839e3c4
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-02 12:19:12 +0000

    included the POST method

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ const friends = [{
 
 server.on('request', (req, res) => {
     const items = req.url.split('/');
-    if (items[1] === 'friends') {
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request', friend);
+            friends.push(JSON.parse(friend));
+        })
+    } else if (items[1] === 'friends') {
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
         // });
